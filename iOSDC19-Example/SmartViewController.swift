c653dd138aa7c5657d8d350dac8378bd8dd2b291
noppefoxwolf <noppelabs@gmail.com>
2019-08-22 03:39:23 +0900

    filter

--- iOSDC19-Example/SmartViewController.swift
+++ iOSDC19-Example/SmartViewController.swift
@@ -14,9 +14,10 @@ import AR2DFaceDetector
 
 class SmartViewController: UIViewController {
   let session: ARSession = .init()
-  let warpGeometryFilter: WarpGeometryFilter = .init(device: MTLCreateSystemDefaultDevice()!)
   let imageView: UIImageView = .init()
   let toggle: UISwitch = .init()
+  let filter: SmartFilter = .init()
+  let context = CIContext()
   
   override func loadView() {
     super.loadView()
@@ -55,69 +56,83 @@ extension SmartViewController: ARSessionDelegate {
   func session(_ session: ARSession, didUpdate frame: ARFrame) {
     let inputImage = CIImage(cvImageBuffer: frame.capturedImage).oriented(.right)
     let detector = AR2DFaceDetector(frame: frame, orientation: .right)
-    if let face = detector.faces.first, let perspectivePoints = face.perspectivePoints, toggle.isOn {
-      let topLeft = perspectivePoints.topLeft.pointInImage(imageSize: inputImage.extent.size).reversedY(height: inputImage.extent.height).vector
-      let topRight = perspectivePoints.topRight.pointInImage(imageSize: inputImage.extent.size).reversedY(height: inputImage.extent.height).vector
-      let bottomRight = perspectivePoints.bottomRight.pointInImage(imageSize: inputImage.extent.size).reversedY(height: inputImage.extent.height).vector
-      let bottomLeft = perspectivePoints.bottomLeft.pointInImage(imageSize: inputImage.extent.size).reversedY(height: inputImage.extent.height).vector
-      
-      let perspectiveCorrectionOutput: CIImage
-      perspectiveCorrection: do {
-        let filter = CIFilter(name: "CIPerspectiveCorrection")!
-        filter.setValue(inputImage.clampedToExtent(), forKey: kCIInputImageKey)
-        filter.setValue(topLeft, forKey: "inputTopLeft")
-        filter.setValue(topRight, forKey: "inputTopRight")
-        filter.setValue(bottomRight, forKey: "inputBottomRight")
-        filter.setValue(bottomLeft, forKey: "inputBottomLeft")
-        perspectiveCorrectionOutput = filter.outputImage!
-      }
-      
-      let warpGeometryOutput: CIImage
-      warpGeometry: do {
-        let sourcePositions: [SIMD2<Float>] = [
-          .init(0.00, 0.00), .init(0.10, 0.00), .init(0.90, 0.00), .init(1.00, 0.00),
-          .init(0.00, 0.25), .init(0.10, 0.25), .init(0.90, 0.25), .init(1.00, 0.25),
-          .init(0.00, 0.55), .init(0.10, 0.55), .init(0.90, 0.55), .init(1.00, 0.55),
-          .init(0.00, 1.00), .init(0.10, 1.00), .init(0.90, 1.00), .init(1.00, 1.00),
-        ]
-        
-        let horizontalDestinationPositions: [SIMD2<Float>] = [
-          .init(0.00, 0.00), .init(0.10, 0.00), .init(0.90, 0.00), .init(1.00, 0.00),
-          .init(0.00, 0.25), .init(0.13, 0.25), .init(0.87, 0.25), .init(1.00, 0.25),
-          .init(0.00, 0.55), .init(0.10, 0.55), .init(0.90, 0.55), .init(1.00, 0.55),
-          .init(0.00, 1.00), .init(0.10, 1.00), .init(0.90, 1.00), .init(1.00, 1.00),
-        ]
-        let warpGeometry = SKWarpGeometryGrid(columns: 3, rows: 3,
-                                              sourcePositions: sourcePositions,
-                                              destinationPositions: horizontalDestinationPositions)
-        warpGeometryFilter.setValue(perspectiveCorrectionOutput, forKey: kCIInputImageKey)
-        warpGeometryFilter.setValue(warpGeometry, forKey: kCIInputWarpGeometryKey)
-        warpGeometryOutput = warpGeometryFilter.outputImage!
-      }
-      
-      let perspectiveTransformOutput: CIImage
-      perspectiveTransform: do {
-        let filter = CIFilter(name: "CIPerspectiveTransform")!
-        filter.setValue(warpGeometryOutput, forKey: kCIInputImageKey)
-        filter.setValue(topLeft, forKey: "inputTopLeft")
-        filter.setValue(topRight, forKey: "inputTopRight")
-        filter.setValue(bottomRight, forKey: "inputBottomRight")
-        filter.setValue(bottomLeft, forKey: "inputBottomLeft")
-        perspectiveTransformOutput = filter.outputImage!
-      }
-      
-      let compositedOutput: CIImage
-      composited: do {
-        compositedOutput = perspectiveTransformOutput.composited(over: inputImage).cropped(to: inputImage.extent)
-      }
-      
-      let context = CIContext()
-      let cgImage = context.createCGImage(compositedOutput, from: compositedOutput.extent)!
+    if let perspectivePoints = detector.faces.first?.perspectivePoints, toggle.isOn {
+      filter.inputImage = inputImage
+      filter.perspectivePoints = perspectivePoints
+      let outputImage: CIImage = filter.outputImage!
+      let cgImage = context.createCGImage(outputImage, from: outputImage.extent)!
       imageView.image = UIImage(cgImage: cgImage)
     } else {
       let context = CIContext()
       let cgImage = context.createCGImage(inputImage, from: inputImage.extent)!
       imageView.image = UIImage(cgImage: cgImage)
     }
+  }
+}
+
+class SmartFilter: CIFilter {
+  @objc var inputImage: CIImage? = nil
+  var perspectivePoints: ARPerspectivePoints2D? = nil
+  private let warpGeometryFilter: WarpGeometryFilter = .init(device: MTLCreateSystemDefaultDevice()!)
+  
+  override var outputImage: CIImage? {
+    guard let inputImage = inputImage else { return nil }
+    guard let perspectivePoints = perspectivePoints else { return inputImage }
+    let topLeft = perspectivePoints.topLeft.pointInImage(imageSize: inputImage.extent.size).reversedY(height: inputImage.extent.height).vector
+    let topRight = perspectivePoints.topRight.pointInImage(imageSize: inputImage.extent.size).reversedY(height: inputImage.extent.height).vector
+    let bottomRight = perspectivePoints.bottomRight.pointInImage(imageSize: inputImage.extent.size).reversedY(height: inputImage.extent.height).vector
+    let bottomLeft = perspectivePoints.bottomLeft.pointInImage(imageSize: inputImage.extent.size).reversedY(height: inputImage.extent.height).vector
+    
+    let perspectiveCorrectionOutput: CIImage
+    perspectiveCorrection: do {
+      let filter = CIFilter(name: "CIPerspectiveCorrection")!
+      filter.setValue(inputImage.clampedToExtent(), forKey: kCIInputImageKey)
+      filter.setValue(topLeft, forKey: "inputTopLeft")
+      filter.setValue(topRight, forKey: "inputTopRight")
+      filter.setValue(bottomRight, forKey: "inputBottomRight")
+      filter.setValue(bottomLeft, forKey: "inputBottomLeft")
+      perspectiveCorrectionOutput = filter.outputImage!
+    }
+    
+    let warpGeometryOutput: CIImage
+    warpGeometry: do {
+      let sourcePositions: [SIMD2<Float>] = [
+        .init(0.00, 0.00), .init(0.10, 0.00), .init(0.90, 0.00), .init(1.00, 0.00),
+        .init(0.00, 0.25), .init(0.10, 0.25), .init(0.90, 0.25), .init(1.00, 0.25),
+        .init(0.00, 0.55), .init(0.10, 0.55), .init(0.90, 0.55), .init(1.00, 0.55),
+        .init(0.00, 1.00), .init(0.10, 1.00), .init(0.90, 1.00), .init(1.00, 1.00),
+      ]
+      
+      let horizontalDestinationPositions: [SIMD2<Float>] = [
+        .init(0.00, 0.00), .init(0.10, 0.00), .init(0.90, 0.00), .init(1.00, 0.00),
+        .init(0.00, 0.25), .init(0.13, 0.25), .init(0.87, 0.25), .init(1.00, 0.25),
+        .init(0.00, 0.55), .init(0.10, 0.55), .init(0.90, 0.55), .init(1.00, 0.55),
+        .init(0.00, 1.00), .init(0.10, 1.00), .init(0.90, 1.00), .init(1.00, 1.00),
+      ]
+      let warpGeometry = SKWarpGeometryGrid(columns: 3, rows: 3,
+                                            sourcePositions: sourcePositions,
+                                            destinationPositions: horizontalDestinationPositions)
+      warpGeometryFilter.setValue(perspectiveCorrectionOutput, forKey: kCIInputImageKey)
+      warpGeometryFilter.setValue(warpGeometry, forKey: kCIInputWarpGeometryKey)
+      warpGeometryOutput = warpGeometryFilter.outputImage!
+    }
+    
+    let perspectiveTransformOutput: CIImage
+    perspectiveTransform: do {
+      let filter = CIFilter(name: "CIPerspectiveTransform")!
+      filter.setValue(warpGeometryOutput, forKey: kCIInputImageKey)
+      filter.setValue(topLeft, forKey: "inputTopLeft")
+      filter.setValue(topRight, forKey: "inputTopRight")
+      filter.setValue(bottomRight, forKey: "inputBottomRight")
+      filter.setValue(bottomLeft, forKey: "inputBottomLeft")
+      perspectiveTransformOutput = filter.outputImage!
+    }
+    
+    let compositedOutput: CIImage
+    composited: do {
+      compositedOutput = perspectiveTransformOutput.composited(over: inputImage).cropped(to: inputImage.extent)
+    }
+    
+    return compositedOutput
   }
 }
